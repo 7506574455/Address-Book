bf37dd78371ffd5693792c9031a28a348abed06f
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-06-23 07:24:44 -0700

    Added: welcome to address book program

---
+++ AddressBookSystem.java
@@ -0,0 +1,8 @@
+package addressbook;
+
+public class AddressBookSystem {
+
+	   public static void main(String[] args) {
+	           System.out.println("welcome to Employee Wage Computation Program");
+	   }
+	}
