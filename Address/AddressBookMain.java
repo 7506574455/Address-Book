70754a0eefeb60efd9c61a3619d76f7b200b3a39
7506574455 <sanketlalge09@gmail.com>
2021-07-21 12:06:31 -0700

    Added: contact person count city or state addressbook program

---
+++ Address/AddressBookMain.java
@@ -0,0 +1,46 @@
+package addressbook;
+
+import java.util.Scanner;
+           //class created
+public class AddressBookMain {
+	
+	private static Scanner scan = new Scanner(System.in);
+	     //main method
+	public static void main(String[] args) {
+		// Displaying Welcome Message
+		System.out.println("Welcome to Address Book Program");
+		// Creating an object of Class AddressBook
+		AddressRunner addressBook = new AddressRunner();
+		// Calling methods from class AddressRunner 
+		boolean choice=true;
+		while (choice) {
+			System.out.println("\nChoose the option:\nPress '1' for Add Contact Details\nPress '2' for Edit Contact Deatils"
+					+ "\nPress '3' for Delete Contact Details\nPress '4' for Show Contact Details\nPress '5' for Contact list by City State"
+					+ "\nPress '6' for Show Contact list of Persons\nPress '7' for Count of contacts by City State\nPress '0' for Exit");
+			int option=scan.nextInt();
+			switch (option) {
+			case 0: choice=false;
+				System.out.println("Exited out of AddressBook");
+				break;
+			case 1: addressBook.addDetails();
+				break;
+			case 2: addressBook.editDetails();
+				break;
+			case 3: addressBook.deleteDetails();
+				break;
+			case 4: addressBook.showDetails();
+				break;
+			case 5: addressBook.contactBycity();
+				break;
+			case 6: addressBook.displayContacts();
+				break;
+			case 7: addressBook.contactsCount();
+				break;
+			default: 
+				System.out.println("Please enter valid input");
+			}
+		}
+	}
+
+}
+
